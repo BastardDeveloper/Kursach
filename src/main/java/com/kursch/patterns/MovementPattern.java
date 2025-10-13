bbb04cd6e9d9d4e20f7f111600baef871638ceb3
BastardDeveloper <tety0090@gmail.com>
2025-10-13 04:52:32 +0300

    чистка

--- src/main/java/com/kursch/patterns/MovementPattern.java
+++ src/main/java/com/kursch/patterns/MovementPattern.java
@@ -5,11 +5,6 @@ import com.badlogic.gdx.math.Vector2;
 public interface MovementPattern {
     Vector2 getPosition(float time);
 
-    /**
-     * Optional: whether the pattern is complete at given time (e.g., entry
-     * finished).
-     * Default: not complete.
-     */
     default boolean isComplete(float time) {
         return false;
     }
